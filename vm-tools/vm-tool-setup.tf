31c7130b2ccbd37f9353015cf7da0971ec6a510b
technical-cloud <technical.profile.cloud@gmail.com>
2025-10-12 13:19:30 +0530

    pipeline-23

--- vm-tools/vm-tool-setup.tf
+++ vm-tools/vm-tool-setup.tf
@@ -4,11 +4,13 @@ variable "vm_public_ip" {
 }
 
 resource "null_resource" "install_devops_tools" {
+  depends_on = [azurerm_linux_virtual_machine.vipin_vm_local]
+
   connection {
-    type        = "ssh"
-    host        = var.vm_public_ip
-    user        = "docker"
-    password    = "Docker@12345"
+    type     = "ssh"
+    host     = azurerm_linux_virtual_machine.vipin_vm_local[0].public_ip_address
+    user     = "docker"
+    password = "Docker@12345"
   }
 
   provisioner "file" {
