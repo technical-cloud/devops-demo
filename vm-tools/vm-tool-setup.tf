6f053286842f6b13919e2c701c99754899f2949a
technical-cloud <technical.profile.cloud@gmail.com>
2025-10-12 13:47:28 +0530

    pipeline-26

--- vm-tools/vm-tool-setup.tf
+++ vm-tools/vm-tool-setup.tf
@@ -1,5 +1,5 @@
 variable "vm_public_ip" {
-  description = "Public IP of the Ubuntu VM"
+  description = "Public IP of the created VM"
   type        = string
 }
 
@@ -8,21 +8,18 @@ resource "null_resource" "install_devops_tools" {
     type     = "ssh"
     host     = var.vm_public_ip
     user     = "docker"
-    password = "Docker@12345"
+    password = "Docker@12345"   # Or use key authentication
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/cloudinit.sh"
+    destination = "/tmp/cloudinit.sh"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update -y",
-      "sudo apt-get install -y git unzip curl docker.io apt-transport-https software-properties-common",
-      "sudo systemctl enable docker",
-      "sudo systemctl start docker",
-      "sudo usermod -aG docker docker",
-      "curl -sL https://aka.ms/InstallAzureCLIDeb | bash",
-      "curl -fLO https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
-      "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",
-      "rm -f kubectl",
-      "curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+      "sudo chmod +x /tmp/cloudinit.sh",
+      "sudo /tmp/cloudinit.sh"
     ]
   }
 }
