8fa096c0e40109de83b9c5c0bb59a580607c2292
technical-cloud <technical.profile.cloud@gmail.com>
2025-10-13 19:09:31 +0530

    pipeline-87

--- ansible-aks-setup/variables.tf
+++ ansible-aks-setup/variables.tf
@@ -1,6 +1,25 @@
 # --------------------------------------------------------------------
 # variables.tf — Define all variables once
 # --------------------------------------------------------------------
+var "subscription_id" {
+description = "Subscription ID"
+type = string
+}
+
+var "tenant_id" {
+description = "Tenant ID"
+type  = string
+}
+
+var "client_id" {
+description = "Client ID" 
+type  = string
+}
+
+var "client_secret" {
+description = "Client Secret"
+type  = string
+}
 
 variable "vm_public_ip" {
   description = "Public IP of the Ubuntu VM"
